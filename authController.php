8f83cadb0de019b63f287938af0e01d1e27f44d3
thotakurasai <53036884+thotakurasai@users.noreply.github.com>
2019-11-18 19:30:44 +0530

    Add files via upload
    create table users
    (
    id int primary key auto_increment,
    username varchar(30),
    email varchar(30),
    verified tinyint,
    token varchar(100),
    password varchar(255)
    );

---
+++ authController.php
@@ -0,0 +1,135 @@
+<?php
+
+session_start();
+
+require 'db.php';
+
+$errors = array();
+$username="";
+$email="";
+
+	if(isset($_POST['signups-btn']))
+	{
+		$username= $_POST['username'];
+		$email= $_POST['email'];
+		$password= $_POST['password'];
+		$passwordConf= $_POST['passwordConf'];
+		
+		if (empty($username))
+		{
+			$errors['username'] = "Username Required";
+		}
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+			$errors['email']="Email address is invalid";
+		}
+		if (empty($email))
+		{
+			$errors['Email'] = "Email Required";
+		}
+		if (empty($password))
+		{
+			$errors['password'] = "Password Required";
+		}
+		if($password !== $passwordConf){
+					$errors['password']= "password's Didn't Match";
+					
+				}
+		$emailQuery="select * from users where email=? limit 1";
+		$stmt = $conn->prepare($emailQuery);
+		$stmt->bind_param('s',$email);
+		$stmt->execute();
+		$result=$stmt->get_result();
+		$userCount = $result->num_rows;
+		$stmt->close();
+		
+		if($userCount>0)
+		{
+			$errors['email']="email already exists";
+		}
+		if(count($errors) === 0)
+		{
+			$password = password_hash($password, PASSWORD_DEFAULT);
+			$token = bin2hex(random_bytes(50));
+			$verified = false;
+			
+			$sql = "insert into users(username,email,verified,token,password) values(?,?,?,?,?)";
+			$stmt = $conn->prepare($sql);
+			$stmt->bind_param('ssbss',$username,$email,$verified,$token,$password);
+			if($stmt->execute())
+			{
+				$user_id = $conn->insert_id;
+				$_SESSION['id']=$user_id;
+				$_SESSION['username']=$username;
+				$_SESSION['email']=$email;
+				$_SESSION['verified']=$verified;
+				
+				$_SESSION['message'] = "you are now logged in";
+				$_SESSION['alert-class'] = "alert-success";
+				header('location:homepage.php');
+				exit();
+			}
+			else{
+				$errors['db_error'] = "Database error: failed to register";
+			}
+			
+		}
+
+	}
+	
+	
+	if(isset($_POST['login-btn']))
+	{
+		$username= $_POST['username'];
+		$password= $_POST['gender'];
+		
+		if (empty($username))
+		{
+			$errors['username'] = "Username Required";
+		}
+		if (empty($password))
+		{
+			$errors['password'] = "Password Required";
+		}
+		if(count($errors) ===0)
+		{
+			$sql = "select * from users where email=? or username=? limit 1";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param('ss',$username,$username);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$user = $result->fetch_assoc();
+		
+		if (password_verify($password,$user['password']))
+		{
+			$_SESSION['id']=$user['id'];
+			$_SESSION['username']=$user['username'];
+			$_SESSION['email']=$user['email'];
+			$_SESSION['verified']=$user['verified'];
+			
+			$_SESSION['message'] = "you are now logged in";
+			$_SESSION['alert-class'] = "alert-success";
+			header('location:homepage.php');
+			exit();
+			
+			}
+			else{
+				$errors['login_fail'] = "Wrong Credentials";
+			}
+			
+		}
+		
+		
+		
+		if (isset($_GET['logout'])){
+			session_destroy();
+			unset($_SESSION['id']);
+			unset($_SESSION['username']);
+			unset($_SESSION['email']);
+			unset($_SESSION['verified']);
+			header('location: login.php');
+			exit();
+		}
+		
+	}
+?>
+	
